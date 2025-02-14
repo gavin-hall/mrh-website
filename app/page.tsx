baa8d3e70f83ecc865a186bc599827a75aee0bfc
Gavin Hall <gavin@magnet.co>
2025-02-13 20:37:56 -0500

    logo and button start

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Mail } from "lucide-react"
+import { Logo } from "@/components/logo"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+        <div className="bg-gray-900">
+        <Logo width={200} height={50} className="text-blue-500" />
+        </div>
+
+        <Button variant="outline"><Mail />Button</Button>
+
+        
         <Image
           className="dark:invert"
           src="/next.svg"
